42849513c8e8d30970725f84dd85ce41c81d6ef0
damiandudycz <damiandudycz@yahoo.com>
2023-12-20 17:09:42 +0100

    Update install_custom_init_scripts.sh

--- installer/scripts/install_custom_init_scripts.sh
+++ installer/scripts/install_custom_init_scripts.sh
@@ -6,7 +6,7 @@
 
 for script in "${guest_init_scripts[@]}"; do
     # Download and install init script
-    local url_script="$url_repo/init.d/$script"
+    local url_script="$url_installer/init.d/$script"
     local path_script="$path_chroot/etc/init.d/$script"
     try wget "$url_script" -O "$path_script" --no-http-keep-alive --no-cache --no-cookies $quiet_flag
     try chmod +x "$path_script"

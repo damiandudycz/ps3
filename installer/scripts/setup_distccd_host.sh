be44bce9ddeacea986ad37f24e27d34c75a24d04
damiandudycz <damiandudycz@yahoo.com>
2023-12-24 12:00:33 +0100

    Update setup_distccd_host.sh

--- installer/scripts/setup_distccd_host.sh
+++ installer/scripts/setup_distccd_host.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Configures host distcc environment.
+# To be used on PS3 Helper Host. Not for PS3 itself.
 
 for allow in "${distccd_host_allow[@]}"; do
     echo 'DISTCCD_OPTS="${DISTCCD_OPTS} --allow '${allow}'"' >> "$path_chroot/etc/conf.d/distccd"

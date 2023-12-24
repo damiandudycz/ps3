93a914802d1e1667965bfdcc64e5fa95241cac12
damiandudycz <damiandudycz@yahoo.com>
2023-12-24 12:47:24 +0100

    Update rebuild_whole_system.sh

--- installer/scripts/rebuild_whole_system.sh
+++ installer/scripts/rebuild_whole_system.sh
@@ -1,6 +1,4 @@
 # Rebuilds entire system from the begining
-# Before starting cleans all binpkgs packages
 # Used by Builder tool, to prepare all packages as binpkgs.
 
-chroot_call "rm -rf /var/cache/binpkgs"
 chroot_call "emerge @system @world --deep --emptytree --with-bdeps=y $quiet_flag"

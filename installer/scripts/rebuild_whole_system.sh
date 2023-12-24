d101a4e8e4117c5db6bfbe6fd6b566750af98225
damiandudycz <damiandudycz@yahoo.com>
2023-12-24 12:51:50 +0100

    Update rebuild_whole_system.sh

--- installer/scripts/rebuild_whole_system.sh
+++ installer/scripts/rebuild_whole_system.sh
@@ -1,3 +1,5 @@
 # Rebuilds entire system from the begining
 
 chroot_call "emerge @system @world --deep --emptytree --with-bdeps=y $quiet_flag"
+
+# TODO: Run dispatch-conf and discard all changes from installed packages, keep already changed files intact.

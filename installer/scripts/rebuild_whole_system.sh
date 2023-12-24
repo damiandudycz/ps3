9f0e25b1c8b28867d83262570971b3035bc6be7d
damiandudycz <damiandudycz@yahoo.com>
2023-12-24 12:49:56 +0100

    Update rebuild_whole_system.sh

--- installer/scripts/rebuild_whole_system.sh
+++ installer/scripts/rebuild_whole_system.sh
@@ -1,4 +1,3 @@
 # Rebuilds entire system from the begining
-# Used by Builder tool, to prepare all packages as binpkgs.
 
 chroot_call "emerge @system @world --deep --emptytree --with-bdeps=y $quiet_flag"

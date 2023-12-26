8e1dd23aeafb25535a34715cf6ff2fb95f754086
Damian Dudycz <damiandudycz@yahoo.com>
2023-12-26 11:55:32 +0100

    Cleaning

--- installer/scripts/create_backup.sh
+++
@@ -1,4 +0,0 @@
-# Creates backup copy of entire system as a tar file after installation is completed
-# Run after unprepare_chroot
-
-try tar cvpJf "$path_chroot/gentoo-ps3.tar.bz" --exclude="$path_chroot/gentoo-ps3.tar.bz" --exclude="$path_chroot/var/cache/binpkgs" -C "$path_chroot"

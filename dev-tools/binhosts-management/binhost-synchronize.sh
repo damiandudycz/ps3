2b5edac2e7e32d72a97f31b6c90a15224ed5ea05
Damian Dudycz <damiandudycz@yahoo.com>
2023-12-26 11:43:59 +0100

    Tools update

---
+++ dev-tools/binhosts-management/binhost-synchronize.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+config="PS3"
+path="../../local/binhost-maintainers/$config"
+path_repo="../../binhosts/$config"
+
+prepare_chroot() {
+	mount --type proc /proc "$path/proc"
+	mount --rbind /sys "$path/sys"
+	mount --make-rslave "$path/sys"
+	mount --rbind /dev "$path/dev"
+	mount --make-rslave "$path/dev"
+	mount --bind /run "$path/run"
+	mount --make-slave "$path/run"
+	cp --dereference '/etc/resolv.conf' "$path/etc/resolv.conf"
+}
+
+unprepare_chroot() {
+	umount -l "$path/dev"{"/shm","/pts"}
+	umount -R "$path/proc"
+	umount -R "$path/run"
+	umount -R "$path/dev"
+	umount -R "$path/sys"
+	rm "$path/etc/resolv.conf"
+}
+
+# Sync and build all packages.
+prepare_chroot
+mount -o bind "$path/var/cache/binpkgs" "$path_repo"
+
+chroot "$path" /bin/bash -c "emerge --sync"
+chroot "$path" /bin/bash -c "FEATURES=\"buildpkg distcc\" emerge --update --newuse --deep --quiet @world @system"
+
+if [[ -n $(git status --porcelain -- "$path_repo") ]]; then
+	git add "$path_repo"/*
+	git commit --path="$path_repo" -m "PS3 Binhost packages update"
+	# TODO: Git push?
+fi
+
+umount "$path_repo"
+unprepare_chroot

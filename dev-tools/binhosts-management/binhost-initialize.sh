5e2353c15c0d07e7813ae26f559eff9ed573d20c
damiandudycz <damiandudycz@yahoo.com>
2023-12-27 18:32:18 +0100

    Update binhost-initialize.sh
    Apply distcc during initial installation

--- dev-tools/binhosts-management/binhost-initialize.sh
+++ dev-tools/binhosts-management/binhost-initialize.sh
@@ -12,7 +12,7 @@ distcc_host="192.168.86.67"
 path="../../local/binhost-maintainers/$config"
 path_repo = "../../binhosts/$config"
 
-../../installer/installer.sh --directory "$path" --config $config
+../../installer/installer.sh --directory "$path" --config $config --distcc "$distcc_host"
 
 prepare_chroot() {
 	mount --type proc /proc "$path/proc"
@@ -37,7 +37,6 @@ unprepare_chroot() {
 # Rebuild all packages and create binpkgs. Add distcc config before that.
 prepare_chroot
 mount -o bind "$path_repo" "$path/var/cache/binpkgs"
-chroot "$path" /bin/bash -c "distcc-config --set-hosts $distcc_host"
 chroot "$path" /bin/bash -c "FEATURES=\"buildpkg distcc\" emerge @system @world --deep --emptytree --with-bdeps=y --quiet"
 umount "$path/var/cache/binpkgs"
 unprepare_chroot

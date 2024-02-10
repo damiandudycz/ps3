48a0307e651714c3cb9a32b3e69740292a1a3a41
Damian Dudycz <damiandudycz@yahoo.com>
2024-02-10 06:35:20 +0000

    Binhost tools update

--- dev-tools/binhosts-management/binhost-update.sh
+++ dev-tools/binhosts-management/binhost-update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Creates clean installations for managing binhost repository.
-
-# TODO: Automatically remove unused variables from config - networking, rc, user, etc
+# Updates packages in binhosts.
 
 prepare_chroot() {
 	local path="$1"
@@ -28,48 +26,42 @@ unprepare_chroot() {
 
 # -----------------------------------------------------------------------------------
 
-distcc="192.168.86.53"
-
 name_base="base"
 path_local_base="../../local/binhost-maintainers/${name_base}"
 path_binhost_base="../../binhosts/ps3-gentoo-binhosts/${name_base}"
-profile_base="default/linux/ppc64/17.0"
 
 name_desktop="desktop"
 path_local_desktop="../../local/binhost-maintainers/${name_desktop}"
 path_binhost_desktop="../../binhosts/ps3-gentoo-binhosts/${name_desktop}"
-profile_desktop="default/linux/ppc64/17.0/desktop"
 
-# Clean old installations.
-rm -rf "${path_local_base}"
-rm -rf "${path_local_desktop}"
-
-# Initialize Base binrepo installation.
-../../installer/installer.sh --directory "${path_local_base}" --edit-config --distcc ${distcc}
 # Mount chroot.
 prepare_chroot "${path_local_base}"
-# Change profile
-chroot "${path_local_base}" /bin/bash -c "eselect profile set ${profile_base}"
-# Reset distcc host to localhost.
-chroot "${path_local_base}" /bin/bash -c "distcc-config --set-hosts 127.0.0.1"
 # Rebuild packages in base binrepo.
 mount -o bind "$path_binhost_base" "$path_local_base/var/cache/binpkgs"
-rm -rf "$path_local_base/var/cache/binpkgs"/* # Delete previous database of binpkgs to get a fresh start
-chroot "$path_local_base" /bin/bash -c "FEATURES=\"buildpkg\" emerge @world --deep --emptytree --with-bdeps=y --binpkg-respect-use=y --quiet"
+chroot "$path_local_base" /bin/bash -c "FEATURES=\"buildpkg\" emerge --sync --quiet"
+chroot "$path_local_base" /bin/bash -c "FEATURES=\"buildpkg\" emerge @world --deep --update --newuse --with-bdeps=y --binpkg-respect-use=y --quiet"
+if [[ -n $(git status --porcelain -- "$path_binhost_base") ]]; then
+	git add "$path_binhost_base"/*
+	git commit --path="$path_binhost_base" -m "PS3 Binhost packages update"
+ 	git push
+fi
 umount "$path_local_base/var/cache/binpkgs"
 # Umount chroot.
 unprepare_chroot "${path_local_base}"
 
-# Initialize PS3 Desktop binrepo installation.
-cp -a "${path_local_base}" "${path_local_desktop}"
 # Mount chroot.
 prepare_chroot "${path_local_desktop}"
 # Change profile
 chroot "${path_local_desktop}" /bin/bash -c "eselect profile set ${profile_desktop}"
 # Rebuild packages in desktop binrepo.
 mount -o bind "$path_binhost_desktop" "$path_local_desktop/var/cache/binpkgs"
-rm -rf "$path_local_desktop/var/cache/binpkgs"/* # Delete previous database of binpkgs to get a fresh start
+chroot "$path_local_base" /bin/bash -c "FEATURES=\"buildpkg\" emerge --sync --quiet"
 chroot "$path_local_desktop" /bin/bash -c "FEATURES=\"buildpkg\" emerge @world --deep --update --newuse --with-bdeps=y --binpkg-respect-use=y --quiet"
+if [[ -n $(git status --porcelain -- "$path_binhost_desktop") ]]; then
+	git add "$path_binhost_desktop"/*
+	git commit --path="$path_binhost_desktop" -m "PS3 Binhost packages update"
+ 	git push
+fi
 umount "$path_local_desktop/var/cache/binpkgs"
 # Umount chroot.
 unprepare_chroot "${path_local_desktop}"

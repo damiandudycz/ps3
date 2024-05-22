b7addb623275d3bafb90193aa21907538f77ac0e
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-22 22:24:52 +0000

    Bind script

---
+++ dev-tools/binhost/binhost-bind.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# This script binds or unbinds the repository binhost directory to/from the catalyst packages directory.
+
+# Function to print error message and exit
+die() {
+    echo "$1" >&2
+    exit 1
+}
+
+# Function to show usage options
+usage() {
+    echo "Usage: $0 --bind | --unbind"
+    exit 1
+}
+
+# Paths
+readonly PATH_START=$(dirname "$(realpath "$0")") || die
+readonly PATH_ROOT=$(realpath -m "${PATH_START}/../..") || die
+readonly PATH_ENV_READY="${PATH_ROOT}/local/env_ready"
+readonly PATH_CATALYST_PACKAGES="/var/tmp/catalyst/packages/default"
+readonly PATH_REPO_BINHOST="${PATH_ROOT}/binhosts/ps3-gentoo-binhosts/default"
+
+# Check if environment is ready
+[ -f "${PATH_ENV_READY}" ] || die "Dev environment was not initialized. Please run dev-tools/setup-environment.sh first."
+
+# Check the arguments
+[ "$#" -ne 1 ] && usage
+
+case "$1" in
+    --bind)
+        # Bind binhost
+        mount -o bind "${PATH_REPO_BINHOST}" "${PATH_CATALYST_PACKAGES}" || die "Failed to mount binhost repo ${PATH_REPO_BINHOST} to ${PATH_CATALYST_PACKAGES}"
+        echo "Successfully mounted ${PATH_REPO_BINHOST} to ${PATH_CATALYST_PACKAGES}"
+        ;;
+    --unbind)
+        # Unbind binhost
+        umount "${PATH_CATALYST_PACKAGES}" || die "Failed to unmount ${PATH_CATALYST_PACKAGES}"
+        echo "Successfully unmounted ${PATH_CATALYST_PACKAGES}"
+        ;;
+    *)
+        # Unsupported option
+        usage
+        ;;
+esac
+
+exit 0
--- dev-tools/release-new-version.sh
+++ dev-tools/release-new-version.sh
@@ -1,18 +1,28 @@
+#!/bin/bash
+
 readonly PATH_START=$(dirname "$(realpath "$0")") || die
 
 declare -a SETUP_SCRIPTS=(
-    release-builder/release-prepare.sh
-    release-builder/release-build.sh
-    release-builder/release-upload.sh
-    binhost/binhost-upload.sh
-    release-builder/release-tag.sh
+    "release-builder/release-prepare.sh"
+    "binhost/binhost-bind.sh --bind"
+    "release-builder/release-build.sh"
+    "release-builder/release-upload.sh"
+    "binhost/binhost-upload.sh"
+    "binhost/binhost-bind.sh --unbind"
+    "release-builder/release-tag.sh"
 )
 
-for SCRIPT in "${SETUP_SCRIPTS[@]}"; do
+for SCRIPT_ENTRY in "${SETUP_SCRIPTS[@]}"; do
+    SCRIPT=$(echo "$SCRIPT_ENTRY" | awk '{print $1}')
+    PARAMS=$(echo "$SCRIPT_ENTRY" | awk '{for (i=2; i<=NF; i++) printf $i" "; print ""}')
+
     DIR=$(dirname "${SCRIPT}")
-    SCRIPT_NAME=$(basename ${SCRIPT})
-    (cd "${DIR}" && "./${SCRIPT_NAME}") || { echo "Script ${SCRIPT} failed. Exiting."; exit 1; }
+    SCRIPT_NAME=$(basename "${SCRIPT}")
+
+    (cd "${DIR}" && "./${SCRIPT_NAME}" ${PARAMS}) || { echo "Script ${SCRIPT} failed. Exiting."; exit 1; }
     cd "${PATH_START}"
 done
 
 echo "New release was successfully released!"
+
+exit 0

a70e3c6856753a7bbfedea128b6b4768febe9c79
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-22 17:54:15 +0000

    Setup git user details in setup-environment

--- dev-tools/setup-environment.sh
+++ dev-tools/setup-environment.sh
@@ -7,6 +7,14 @@ declare -a SETUP_SCRIPTS=(
     setup-crossdev.sh
 )
 
+GIT_USER="Damian Dudycz"
+GIT_EMAIL="damiandudycz@yahoo.com"
+
+# Setup git user details
+git config --global user.name "${GIT_USER}"
+git config --global user.email "${GIT_EMAIL}"
+
+# Run setup scripts
 for SCRIPT in "${SETUP_SCRIPTS[@]}"; do
     DIR=$(dirname "environment/${SCRIPT}")
     (cd "${DIR}" && "./${SCRIPT}") || { echo "Script ${SCRIPT} failed. Exiting."; exit 1; }

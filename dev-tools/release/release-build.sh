22d110c550eebec0f34431bc4efd4aeded756c21
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-23 15:48:21 +0000

    Cleaning

--- dev-tools/release/release-build.sh
+++ dev-tools/release/release-build.sh
@@ -22,7 +22,7 @@ trap cleanup EXIT
 readonly PATH_START=$(dirname "$(realpath "$0")") || die
 readonly PATH_ROOT=$(realpath -m "${PATH_START}/../..") || die
 readonly PATH_ENV_READY="${PATH_ROOT}/local/env_ready"
-readonly PATH_LOCAL_TMP="${PATH_ROOT}/local/release-builder"
+readonly PATH_LOCAL_TMP="${PATH_ROOT}/local/release"
 readonly PATH_RELEASE_INFO="${PATH_LOCAL_TMP}/release_latest"
 readonly PATH_INSTALLER_UPDATER="${PATH_ROOT}/dev-tools/ps3-installer/ps3-gentoo-installer-ebuild-updater.sh"
 readonly PATH_BINHOST_BIND="${PATH_ROOT}/dev-tools/binhost/binhost-bind.sh"

d63671a82bd2c4646a7f7aca6b8dc9ca5604f8ad
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-24 15:51:03 +0000

    Compiling ebuild

--- dev-tools/kernel-ebuild/ebuild-06-build-ebuild.sh
+++ dev-tools/kernel-ebuild/ebuild-06-build-ebuild.sh
@@ -11,10 +11,13 @@ readonly PATH_START=$(dirname "$(realpath "$0")") || die "Failed to determine sc
 readonly PATH_ROOT=$(realpath -m "${PATH_START}/../..") || die "Failed to determine root directory."
 readonly PATH_VERSION_SCRIPT="${PATH_START}/ebuild-00-find-version.sh"
 [ ! -z "${PACKAGE_VERSION}" ] || PACKAGE_VERSION=$($PATH_VERSION_SCRIPT) || die "Failed to get default version of package"
+readonly NAME_DISTFILES_FILE="gentoo-kernel-ps3-files-${PACKAGE_VERSION}.tar.xz"
+readonly NAME_EBUILD_FILE="gentoo-kernel-ps3-${PACKAGE_VERSION}.ebuild"
 readonly PATH_WORK="${PATH_ROOT}/local/gentoo-kernel-ps3/${PACKAGE_VERSION}/ebuild"
 readonly PATH_WORK_DISTFILES="${PATH_WORK}/distfiles"
-readonly PATH_WORK_DISTFILES_TAR="${PATH_WORK_DISTFILES}/gentoo-kernel-ps3-files-${PACKAGE_VERSION}.tar.xz"
-readonly PATH_WORK_EBUILD_FILE="${PATH_WORK}/sys-kernel/gentoo-kernel-ps3/gentoo-kernel-ps3-${PACKAGE_VERSION}.ebuild"
+readonly PATH_WORK_DISTFILES_TAR="${PATH_WORK_DISTFILES}/${NAME_DISTFILES_FILE}"
+readonly PATH_WORK_EBUILD_FILE="${PATH_WORK}/sys-kernel/gentoo-kernel-ps3/${NAME_EBUILD_FILE}"
+readonly PATH_EBUILD_MANIFEST="${PATH_WORK}/sys-kernel/gentoo-kernel-ps3/Manifest"
 
 # Verify data.
 [[ "${PACKAGE_VERSION}" =~ ^[0-9]+\.[0-9]+\.[0-9]+([0-9]+)?$ ]] || die "Please provide valid version number, ie. $0 6.6.30"
@@ -24,7 +27,11 @@ readonly PATH_WORK_EBUILD_FILE="${PATH_WORK}/sys-kernel/gentoo-kernel-ps3/gentoo
 
 # Create manifest and digest
 cd "${PATH_WORK}" || die "Failed to open ${PATH_WORK}"
-DISTDIR="${PATH_WORK_DISTFILES}" ebuild sys-kernel/gentoo-kernel-ps3/gentoo-kernel-ps3-${PACKAGE_VERSION}.ebuild manifest || die "Failed to build manifest"
+DISTDIR="${PATH_WORK_DISTFILES}" ebuild sys-kernel/gentoo-kernel-ps3/${NAME_EBUILD_FILE} manifest || die "Failed to build manifest"
+
+# Clear other downloaded distfiles, except gentoo-kernel-ps3-files.
+find "${PATH_WORK_DISTFILES}" ! -name "${NAME_DISTFILES_FILE}" -type f -exec rm {} + || die "Failed to prunt distfiles"
+rm -rf "${PATH_WORK}/profiles" || die "Failed to remove profiles directory"
 
 echo "Gentoo-Kernel-PS3 Ebuild manifest generated successfully."
 exit 0

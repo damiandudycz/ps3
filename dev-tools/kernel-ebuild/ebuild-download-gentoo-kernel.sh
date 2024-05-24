a98d0a9ce17d0883b26c9a41300425eb2b5c8bc5
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-24 09:18:23 +0000

    Cleanup

--- dev-tools/kernel-ebuild/ebuild-download-gentoo-kernel.sh
+++ dev-tools/kernel-ebuild/ebuild-download-gentoo-kernel.sh
@@ -13,6 +13,9 @@ die() {
     exit 1
 }
 
+NAME_KEYWORD=" ppc64"
+PACKAGE_VERSION="$1"
+
 readonly PATH_START=$(dirname "$(realpath "$0")") || die "Failed to determine script directory."
 readonly PATH_ROOT=$(realpath -m "${PATH_START}/../..") || die "Failed to determine root directory."
 readonly PATH_LOCAL="${PATH_ROOT}/local/kernel"
@@ -20,8 +23,6 @@ readonly PATH_VERSION_SCRIPT="${PATH_START}/ebuild-find-version.sh"
 readonly PATH_REPO_GENTOO="/var/db/repos/gentoo"
 readonly NAME_PACKAGE="sys-kernel/gentoo-kernel"
 readonly NAME_EBUILD="gentoo-kernel"
-NAME_KEYWORD=" ppc64"
-PACKAGE_VERSION="$1"
 
 [ ! -z "${PACKAGE_VERSION}" ] || PACKAGE_VERSION=$($PATH_VERSION_SCRIPT) || die "Failed to get default version of package"
 [[ "${PACKAGE_VERSION}" =~ ^[0-9]+\.[0-9]+\.[0-9]+([0-9]+)?$ ]] || die "Please provide valid version number, ie. $0 6.6.30"

1c3bf4b00849949970d4cf149874f6db826e166b
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-24 15:32:14 +0000

    Generating ebuild manifest and digest

--- dev-tools/kernel-ebuild/ebuild-05-create-ps3-ebuild.sh
+++ dev-tools/kernel-ebuild/ebuild-05-create-ps3-ebuild.sh
@@ -26,6 +26,7 @@ readonly PATH_START=$(dirname "$(realpath "$0")") || die "Failed to determine sc
 readonly PATH_ROOT=$(realpath -m "${PATH_START}/../..") || die "Failed to determine root directory."
 readonly PATH_VERSION_STORAGE="${PATH_START}/data/version-storage"
 readonly PATH_VERSION_SCRIPT="${PATH_START}/ebuild-00-find-version.sh"
+readonly PATH_REPO_DRAFT="${PATH_START}/data/repo"
 [ ! -z "${PACKAGE_VERSION}" ] || PACKAGE_VERSION=$($PATH_VERSION_SCRIPT) || die "Failed to get default version of package"
 readonly PATH_VERSION_CONFIG="${PATH_VERSION_STORAGE}/${PACKAGE_VERSION}/config/diffs"
 readonly PATH_VERSION_DEFCONFIG="${PATH_VERSION_STORAGE}/${PACKAGE_VERSION}/config/defconfig"
@@ -33,7 +34,7 @@ readonly PATH_VERSION_PATCHES="${PATH_VERSION_STORAGE}/${PACKAGE_VERSION}/patche
 readonly PATH_EBUILD_PACTHES="${PATH_START}/data/ebuild-patches"
 readonly PATH_PORTAGE_EBUILD_FILE="/var/db/repos/gentoo/${NAME_PACKAGE}/gentoo-kernel-${PACKAGE_VERSION}.ebuild"
 readonly PATH_WORK="${PATH_ROOT}/local/gentoo-kernel-ps3/${PACKAGE_VERSION}/ebuild"
-readonly PATH_WORK_EBUILD="${PATH_WORK}/ebuild"
+readonly PATH_WORK_EBUILD="${PATH_WORK}/${NAME_PACKAGE}-ps3"
 readonly PATH_WORK_DISTFILES="${PATH_WORK}/distfiles"
 readonly PATH_WORK_DISTFILES_TAR="${PATH_WORK_DISTFILES}/gentoo-kernel-ps3-files-${PACKAGE_VERSION}.tar.xz"
 readonly PATH_WORK_EBUILD_FILE="${PATH_WORK_EBUILD}/gentoo-kernel-ps3-${PACKAGE_VERSION}.ebuild"
@@ -50,6 +51,9 @@ mkdir -p "${PATH_WORK}" || die "Failed to create workdir ${PATH_WORK}"
 mkdir -p "${PATH_WORK_EBUILD}" || die "Failed to create workdir ${PATH_WORK_EBUILD}"
 mkdir -p "${PATH_WORK_DISTFILES}" || die "Failed to create workdir ${PATH_WORK_DISTFILES}"
 mkdir -p "${PATH_WORK_DISTFILES}/ps3_patches" || die "Failed to create workdir ${PATH_WORK_DISTFILES}/patches"
+
+# Create empty portage overlay structure.
+cp -rf "${PATH_REPO_DRAFT}"/* "${PATH_WORK}"/
 
 # Geather files.
 cp -f "${PATH_VERSION_PATCHES}"/*.patch "${PATH_WORK_DISTFILES}/ps3_patches"/ || die "Failed to copy patches"
--- dev-tools/kernel-ebuild/ebuild-06-build-ebuild.sh
+++ dev-tools/kernel-ebuild/ebuild-06-build-ebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# This script prepares manifest for previously generated ebuild and distfiles.
-
 die() {
     echo "$*" 1>&2
     exit 1
@@ -14,15 +12,19 @@ readonly PATH_ROOT=$(realpath -m "${PATH_START}/../..") || die "Failed to determ
 readonly PATH_VERSION_SCRIPT="${PATH_START}/ebuild-00-find-version.sh"
 [ ! -z "${PACKAGE_VERSION}" ] || PACKAGE_VERSION=$($PATH_VERSION_SCRIPT) || die "Failed to get default version of package"
 readonly PATH_WORK="${PATH_ROOT}/local/gentoo-kernel-ps3/${PACKAGE_VERSION}/ebuild"
-readonly PATH_WORK_EBUILD="${PATH_WORK}/ebuild"
 readonly PATH_WORK_DISTFILES="${PATH_WORK}/distfiles"
 readonly PATH_WORK_DISTFILES_TAR="${PATH_WORK_DISTFILES}/gentoo-kernel-ps3-files-${PACKAGE_VERSION}.tar.xz"
-readonly PATH_WORK_EBUILD_FILE="${PATH_WORK_EBUILD}/gentoo-kernel-ps3-${PACKAGE_VERSION}.ebuild"
+readonly PATH_WORK_EBUILD_FILE="${PATH_WORK}/sys-kernel/gentoo-kernel-ps3/gentoo-kernel-ps3-${PACKAGE_VERSION}.ebuild"
 
 # Verify data.
 [[ "${PACKAGE_VERSION}" =~ ^[0-9]+\.[0-9]+\.[0-9]+([0-9]+)?$ ]] || die "Please provide valid version number, ie. $0 6.6.30"
+[ -d "${PATH_WORK}" ] || die "Workdir for version ${PACKAGE_VERSION} not found."
+[ -f "${PATH_WORK_EBUILD_FILE}" ] || die "Ebuild for version ${PACKAGE_VERSION} not found."
+[ -f "${PATH_WORK_DISTFILES_TAR}" ] || die "Distfiles for version ${PACKAGE_VERSION} not found."
 
-# Generate manifest
+# Create manifest and digest
+cd "${PATH_WORK}" || die "Failed to open ${PATH_WORK}"
+DISTDIR="${PATH_WORK_DISTFILES}" ebuild sys-kernel/gentoo-kernel-ps3/gentoo-kernel-ps3-${PACKAGE_VERSION}.ebuild manifest || die "Failed to build manifest"
 
 echo "Gentoo-Kernel-PS3 Ebuild manifest generated successfully."
 exit 0

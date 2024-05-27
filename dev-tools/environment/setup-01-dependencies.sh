fed94a7007470090be1272546ff6229146db0c70
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-27 12:09:03 +0000

    Remove svn

--- dev-tools/environment/setup-01-dependencies.sh
+++ dev-tools/environment/setup-01-dependencies.sh
@@ -7,5 +7,5 @@
 source ../../.env-shared.sh || exit 1
 trap failure ERR
 
-readonly PACKAGES_DEPENDENCIES=(gentoolkit ruby pkgdev dev-vcs/subversion)
+readonly PACKAGES_DEPENDENCIES=(gentoolkit ruby pkgdev)
 emerge --newuse --update --deep "${PACKAGES_DEPENDENCIES[@]}"

7e28a558a730892408c6ff47ab5d5394b8cf218a
Damian Dudycz <damiandudycz@yahoo.com>
2024-05-28 15:24:57 +0000

    Cleanup

--- dev-tools/kernel-ebuild/env.sh
+++ dev-tools/kernel-ebuild/env.sh
@@ -11,8 +11,7 @@ readonly KE_ENV_LOADED=true
 
 # Input parsing.
 while [ $# -gt 0 ]; do case "$1" in
-    --unstable)    KE_FLAG_UNSTABLE=true;;             # Use unstable ebuilds ~ppc64.
-    --unmask)      KE_FLAG_UNMASK=true;;               # Unmask created ps3 ebuild ~ppc64 -> ppc64.
+    --unmask)      KE_FLAG_UNMASK=true;;               # Use masked ~ppc64 base ebuilds and unmask created ps3 ebuild ~ppc64 -> ppc64.
     --default)     KE_FLAG_FORCE_DEFAULT=true;;        # Force using default config and patches, even if version specific data exists.
     --savepatches) KE_FLAG_SAVE_PATCHES=true;;         # Save patches in versioned directory, if used default patches. This option should be always used to keep track of working set of patches.
     --saveconfig)  KE_FLAG_SAVE_CONFIG=true;;          # Save config in versioned directory after ebuild-04-configure.sh. This option sould be always used to store changes made by this script.
@@ -22,11 +21,10 @@ esac; shift; done
 
 # Validate input variables;
 [ ! ${KE_PACKAGE_VERSION_SPECIFIED} ] || [[ "${KE_PACKAGE_VERSION_SPECIFIED}" =~ ^[0-9]+\.[0-9]+\.[0-9]+([0-9]+)?$ ]] || failure "Kernel version incorrect"
-[ ! ${KE_FLAG_UNMASK} ] || [ ${KE_FLAG_UNSTABLE} ] || failure "--unmask option can only be used with --unstable"
 
 # Scripts in kernel-ebuild group.
 readonly KE_SCRIPT_CURRENT="$(basename $0)"                                     # Name of script that was called by the user.
-readonly KE_SCRIPT_FIND_VERSION="ebuild-00-find-version.sh"                     # Finds version of gentoo-kernel - stable or unstable, depending on KE_FLAG_UNSTABLE.
+readonly KE_SCRIPT_FIND_VERSION="ebuild-00-find-version.sh"                     # Finds version of gentoo-kernel - stable or unstable, depending on KE_FLAG_UNMASK.
 readonly KE_SCRIPT_DOWNLOAD_PATCHES="ebuild-01-download-patches.sh"             # Downloads patches and stores in KE_PATH_VERSION_STORAGE. Stores in default or in KE_PACKAGE_VERSION if explicitly specified.
 readonly KE_SCRIPT_DOWNLOAD_GENTOO_KERNEL="ebuild-02-download-gentoo-kernel.sh" # Downloads gentoo-kernel files from gentoo-kernel ebuild.
 readonly KE_SCRIPT_APPLY_PS3_PATCHES="ebuild-03-apply-ps3-patches.sh"           # Applies all PS3 patches available for this gentoo-kernel.
@@ -44,7 +42,7 @@ readonly KE_NAME_EBUILD_FILE_DST="gentoo-kernel-ps3"
 readonly KE_NAME_EBUILD_DEFCONFIG="ps3_defconfig"
 readonly KE_VAR_EBUILD_KEYWORD_DEFAULT="ppc64"
 readonly KE_VAR_EBUILD_KEYWORD_UNSTABLE="~ppc64"
-         KE_VAR_EBUILD_KEYWORD_SELECTED="${KE_VAR_EBUILD_KEYWORD_DEFAULT}"; [ ${KE_FLAG_UNSTABLE} ] && KE_VAR_EBUILD_KEYWORD_SELECTED="${KE_VAR_EBUILD_KEYWORD_UNSTABLE}"
+         KE_VAR_EBUILD_KEYWORD_SELECTED="${KE_VAR_EBUILD_KEYWORD_DEFAULT}"; [ ${KE_FLAG_UNMASK} ] && KE_VAR_EBUILD_KEYWORD_SELECTED="${KE_VAR_EBUILD_KEYWORD_UNSTABLE}"
 
 # Package version.
 KE_PACKAGE_VERSION_DEFAULT=""; [ "${KE_SCRIPT_CURRENT}" = "${KE_SCRIPT_FIND_VERSION}" ] || KE_PACKAGE_VERSION_DEFAULT="$(source $KE_SCRIPT_FIND_VERSION)" || failure # Version of package returned by ebuild-00-find-version.sh.

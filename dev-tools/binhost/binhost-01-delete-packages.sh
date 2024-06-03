cbd080664e56ad887d26fb281ef0eee4dcc9f81a
Damian Dudycz <damiandudycz@yahoo.com>
2024-06-03 22:46:24 +0000

    Added regex search in binrepo dev tools

--- dev-tools/binhost/binhost-01-delete-packages.sh
+++ dev-tools/binhost/binhost-01-delete-packages.sh
@@ -3,36 +3,57 @@
 # This script deletes a specified package from the binhost repository.
 # Usage example: ./binhost-delete-package.sh sys-kernel/gentoo-kernel-ps3.
 # It can also remove packages larger than specified size - this functionality
-# can be used with or without specyfying package name.
+# can be used with or without specifying package name.
 
 source ../../.env-shared.sh || exit 1
-register_usage "$0 <pckage>[-version] | --if-larger <SIZE_LIMIT>"
+register_usage "$0 <package>[-version] | --if-larger <SIZE_LIMIT>"
 
 # Parse input parameters
 declare -a ARG_PACKAGES_TO_REMOVE
-while [[ $# -gt 0 ]]; do case "$1" in
-    --verbose);; # Handled by env-shared.sh
-    --if-larger|-s)
-        SIZE_LIMIT=$2
-        case "${SIZE_LIMIT: -1}" in
-            K|k) SIZE_LIMIT=$(( ${SIZE_LIMIT%K*} * 1024 )) ;;
-            M|m) SIZE_LIMIT=$(( ${SIZE_LIMIT%M*} * 1024 * 1024 )) ;;
-            G|g) SIZE_LIMIT=$(( ${SIZE_LIMIT%G*} * 1024 * 1024 * 1024 )) ;;
-        esac
-        shift 2
-        ;;
-    --*|-*)
-        failure "Unknown parameter: $1"
-        ;;
-    *)
-        ARG_PACKAGES_TO_REMOVE+=("$1")
-        shift
-        ;;
-esac; done
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --verbose) ;; # Handled by env-shared.sh
+        --if-larger|-s)
+            SIZE_LIMIT=$2
+            case "${SIZE_LIMIT: -1}" in
+                K|k) SIZE_LIMIT=$(( ${SIZE_LIMIT%K*} * 1024 )) ;;
+                M|m) SIZE_LIMIT=$(( ${SIZE_LIMIT%M*} * 1024 * 1024 )) ;;
+                G|g) SIZE_LIMIT=$(( ${SIZE_LIMIT%G*} * 1024 * 1024 * 1024 )) ;;
+            esac
+            shift 2
+            ;;
+        --*|-*)
+            failure "Unknown parameter: $1"
+            ;;
+        *)
+            # If the package doesn't contain a category, prepend '*' to match any category
+            if [[ "$1" != */* ]]; then
+                ARG_PACKAGES_TO_REMOVE+=("*/$1")
+            else
+                ARG_PACKAGES_TO_REMOVE+=("$1")
+            fi
+            shift
+            ;;
+    esac
+done
 
 # Check if package parameter is provided and package exists
 [[ ! -z "${ARG_PACKAGES_TO_REMOVE}" ]] || [ ${SIZE_LIMIT} ] || show_usage
 [[ -d "${PATH_BINHOSTS_PS3_GENTOO_DEFAULT}/${CONF_PACKAGE_NAME}" ]] || failure "Package ${CONF_PACKAGE_NAME} not found in ${PATH_BINHOSTS_PS3_GENTOO_DEFAULT}"
+
+# Convert wildcard to regex
+convert_to_regex() {
+    local WILDCARD="$1"
+    WILDCARD="${WILDCARD//\*/.*}"
+    WILDCARD="${WILDCARD//\?/.}"
+    echo "^${WILDCARD}(-[0-9][0-9.a-zA-Z]*)?(_p[0-9]+)?(-r[0-9]+)?$"
+}
+
+# Convert package names with wildcards to regex
+declare -a ARG_PACKAGES_TO_REMOVE_REGEX
+for PACKAGE in "${ARG_PACKAGES_TO_REMOVE[@]}"; do
+    ARG_PACKAGES_TO_REMOVE_REGEX+=("$(convert_to_regex "$PACKAGE")")
+done
 
 # Process metadata file
 PACKAGES_COUNT=$(grep -oP '^PACKAGES: \K[0-9]+' "${PATH_BINHOSTS_PS3_GENTOO_DEFAULT_METADATA}")
@@ -41,39 +62,28 @@ ENTRY=""
 unset ENTRY_DELETE
 unset METADATA_MODIFIED
 while IFS= read -r LINE || [[ -n $LINE ]]; do
-    # New line means we are starting new entry.
     if [[ -z "${LINE}" ]]; then
-	# Process entry
-        ENTRY_PACKAGE=$(awk -F'/' '{print $1"/"$2}' <<< "${ENTRY_PATH}") # sys-kernel/gentoo-kernel-ps3
+        ENTRY_PACKAGE=$(awk -F'/' '{print $1"/"$2}' <<< "${ENTRY_PATH}")
         ENTRY_VERSION="${ENTRY_CPV#"$ENTRY_PACKAGE-"}"
         ENTRY_PACKAGE_VERSIONED="${ENTRY_PACKAGE}-${ENTRY_VERSION}"
-        # Check if package is present in specified packages to delete
-        for PACKAGE_TO_REMOVE in ${ARG_PACKAGES_TO_REMOVE[@]}; do
+        for PACKAGE_REGEX in "${ARG_PACKAGES_TO_REMOVE_REGEX[@]}"; do
             if [[ ! ${SIZE_LIMIT} ]] || [[ ${ENTRY_SIZE} -gt ${SIZE_LIMIT} ]]; then
-                # Check if package to remove equals given package to remove including versioning
-                if [[ "${ENTRY_PACKAGE_VERSIONED}" == "${PACKAGE_TO_REMOVE}"* ]] && [[ "${PACKAGE_TO_REMOVE}" == "${ENTRY_PACKAGE}"* ]]; then
-                    # Checks if version ending charakters are not different.
-                    # This is done, so that setting for example 6.8.1 doesn't remove 6.8.11
-                    REMAINING="${ENTRY_PACKAGE_VERSIONED#${PACKAGE_TO_REMOVE}}"
-                    [[ ! "$REMAINING" =~ ^[0-9] ]] && ENTRY_DELETE=true
+                if [[ "${ENTRY_PACKAGE_VERSIONED}" =~ ${PACKAGE_REGEX} ]]; then
+                    ENTRY_DELETE=true
                 fi
             fi
         done
-        # If only size limit was specified
-        if [[ -z "${ARG_PACKAGES_TO_REMOVE}" ]] && [[ ${ENTRY_SIZE} -gt ${SIZE_LIMIT} ]]; then
+        if [[ -z "${ARG_PACKAGES_TO_REMOVE_REGEX}" ]] && [[ ${ENTRY_SIZE} -gt ${SIZE_LIMIT} ]]; then
             ENTRY_DELETE=true
         fi
         if [[ -n ${ENTRY_DELETE} ]]; then
-            # Package was market to delete
             echo "Removing ${ENTRY_CPV} [${ENTRY_PATH}]"
-            ((PACKAGES_COUNT--))
+            ((PACKAGES_COUNT--)) || PACKAGES_COUNT=0
             METADATA_MODIFIED=true
             rm -f "${PATH_BINHOSTS_PS3_GENTOO_DEFAULT}/${ENTRY_PATH}"
         else
-            # Add entry if shoudn't delete it
             VAR_METADATA_NEW+="${ENTRY}\n"
         fi
-        # Reset variables
         ENTRY=""; ENTRY_CPV=""; ENTRY_PATH=""; ENTRY_SIZE=0; unset ENTRY_DELETE
     else
         ENTRY+="${LINE}\n"

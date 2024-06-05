dd1e49cbe832f5958c6232b35962af170a47f6ee
Damian Dudycz <damiandudycz@yahoo.com>
2024-06-05 07:18:34 +0000

    New development stage - wip

---
+++ dev-tools/overlay/copy-ps3-distfiles.sh
@@ -0,0 +1,3 @@
+
+# This script copies all distfiles available in ps3-gentoo-overlay to /var/cache/distfiles.
+# This is needed for catalyst to be able to grab the newest files, before they are send to github.

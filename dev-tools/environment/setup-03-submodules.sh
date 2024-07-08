f376684314cb349da18d734f99ba7dfdba83d547
Damian Dudycz <damiandudycz@yahoo.com>
2024-07-08 23:29:39 +0000

    Update

--- dev-tools/environment/setup-03-submodules.sh
+++ dev-tools/environment/setup-03-submodules.sh
@@ -20,3 +20,16 @@ for dir in "${PATH_RELEASES_PS3_GENTOO_ARCH_BINPACKAGES_PROFILE}"/{.[!.]*,*}; do
     git add -u
     echo ""
 done
+
+# Find all files larger than 100M and track them with Git LFS
+echo "[Add release files to LFS if needed]"
+cd "/home/gentoo/ps3/releases"
+find . -type f -size +100M | while read file; do
+    if [[ \$file != *".git"* ]]; then
+        git lfs track "\$file"
+        git add "\$file"
+    fi
+done
+git add .gitattributes
+EOF
+chmod +x "${PATH_GIT_HOOK_RELEASES}"

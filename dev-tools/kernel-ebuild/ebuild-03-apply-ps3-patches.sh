5c624246f96b217dc802999ba3225f8b349c66de
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-07 18:13:08 +0000

    Improved kernel-ebuild tools

--- dev-tools/kernel-ebuild/ebuild-03-apply-ps3-patches.sh
+++ dev-tools/kernel-ebuild/ebuild-03-apply-ps3-patches.sh
@@ -3,6 +3,9 @@
 source ../../.env-shared.sh || exit 1
 source "${PATH_EXTRA_ENV_KERNEL_EBUILD}" || { echo "Failed to load env ${PATH_EXTRA_ENV_KERNEL_EBUILD}"; exit 1; }
 
+[[ ! -d "${KE_PATH_WORK_VERSION_PATCHES}" ]] || failure "Patches already applied"
+[[ -d "${KE_PATH_WORK_PATCHES_SRC}" ]] || failure "Pathes source folder not found: ${KE_PATH_WORK_PATCHES_SRC}"
+
 apply_patches() {
     local patch_dir=$1
     for PATCH in "$patch_dir"/*.patch; do
@@ -12,7 +15,10 @@ apply_patches() {
     done
 }
 
+# Link patches
+ln -s "${KE_PATH_WORK_PATCHES_SRC}" "${KE_PATH_WORK_VERSION_PATCHES}"
+
 # Apply PS3 patches.
-find "${KE_PATH_WORK_PATCHES}" -type d | while read -r subdir; do
+find -L "${KE_PATH_WORK_VERSION_PATCHES}" -type d | while read -r subdir; do
     apply_patches "${subdir}"
 done

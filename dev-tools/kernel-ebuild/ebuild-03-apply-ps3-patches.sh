619eb0fd821c8d0498d88ad934fdf4c54b066470
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-07 19:47:11 +0000

    Small improvement for script 03

--- dev-tools/kernel-ebuild/ebuild-03-apply-ps3-patches.sh
+++ dev-tools/kernel-ebuild/ebuild-03-apply-ps3-patches.sh
@@ -4,8 +4,13 @@ source ../../.env-shared.sh || exit 1
 source "${PATH_EXTRA_ENV_KERNEL_EBUILD}" || { echo "Failed to load env ${PATH_EXTRA_ENV_KERNEL_EBUILD}"; exit 1; }
 
 [[ ! -d "${KE_PATH_WORK_VERSION_PATCHES}" ]] || failure "Patches already applied"
-[[ -d "${KE_PATH_WORK_PATCHES_SRC}" ]] || failure "Pathes source folder not found: ${KE_PATH_WORK_PATCHES_SRC}"
 
+if [[ ! -d "${KE_PATH_WORK_PATCHES_SRC}" ]]; then
+	echo "Pathes source folder not found: ${KE_PATH_WORK_PATCHES_SRC}"
+	echo "Available patches sets:"
+	ls "${KE_PATH_WORK_PATCHES}"
+	exit 1
+fi
 apply_patches() {
     local patch_dir=$1
     for PATCH in "$patch_dir"/*.patch; do

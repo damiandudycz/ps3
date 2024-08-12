7711316c91a4672f1eca96ad3f528fe0a63c7b2e
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-12 22:37:44 +0200

    Restore vanilla linux files

--- dev-tools/kernel-patches/linux-files/arch/powerpc/platforms/ps3/system-bus.c
+++ dev-tools/kernel-patches/linux-files/arch/powerpc/platforms/ps3/system-bus.c
@@ -163,7 +163,6 @@ int ps3_open_hv_device(struct ps3_system_bus_device *dev)
 	case PS3_MATCH_ID_STOR_DISK:
 	case PS3_MATCH_ID_STOR_ROM:
 	case PS3_MATCH_ID_STOR_FLASH:
-	case PS3_MATCH_ID_STOR_NOR_FLASH:
 		return ps3_open_hv_device_sb(dev);
 
 	case PS3_MATCH_ID_SOUND:
@@ -202,7 +201,6 @@ int ps3_close_hv_device(struct ps3_system_bus_device *dev)
 	case PS3_MATCH_ID_STOR_DISK:
 	case PS3_MATCH_ID_STOR_ROM:
 	case PS3_MATCH_ID_STOR_FLASH:
-	case PS3_MATCH_ID_STOR_NOR_FLASH:
 		return ps3_close_hv_device_sb(dev);
 
 	case PS3_MATCH_ID_SOUND:

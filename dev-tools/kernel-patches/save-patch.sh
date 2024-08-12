53a28fc8410ef32f4896cde10c4b227a76f55856
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-12 21:47:41 +0200

    FIXES and RESTORE all vanilla

--- dev-tools/kernel-patches/save-patch.sh
+++ dev-tools/kernel-patches/save-patch.sh
@@ -1,7 +1,7 @@
 patch_name="$1"
 git add linux-files
 cd linux-files
-git diff --relative --cached . > ../patches/damiandudycz/$path_name.patch
+git diff --relative --cached . > ../patches/damiandudycz/$patch_name.patch
 cd ..
-git add patches/damiandudycz/$path_name.patch
+git add patches/damiandudycz/$patch_name.patch
 git commit -m "Add kernel patch: $patch_name"

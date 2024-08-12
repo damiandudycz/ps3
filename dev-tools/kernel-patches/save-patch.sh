630b3b4100607d38244c2c11b859c2ee1d7e5e0c
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-12 21:29:32 +0200

    fix scripts

--- dev-tools/kernel-patches/save-patch.sh
+++ dev-tools/kernel-patches/save-patch.sh
@@ -1,7 +1,7 @@
 patch_name="$1"
 git add linux-files
 cd linux-files
-git diff --relative --cached . > ../patches/damiandudycz/$path_name
+git diff --relative --cached . > ../patches/damiandudycz/$path_name.patch
 cd ..
-git add patches/damiandudycz/$path_name
+git add patches/damiandudycz/$path_name.patch
 git commit -m "Add kernel patch: $patch_name"

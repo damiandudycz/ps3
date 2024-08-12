b2a4824b69f982313b32334f040e4ac4749fcc9b
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-12 23:24:19 +0200

    Restore vanilla linux files

--- dev-tools/kernel-patches/linux-files/arch/powerpc/platforms/ps3/system-bus.c
+++ dev-tools/kernel-patches/linux-files/arch/powerpc/platforms/ps3/system-bus.c
@@ -163,7 +163,6 @@ int ps3_open_hv_device(struct ps3_system_bus_device *dev)
 	case PS3_MATCH_ID_STOR_DISK:
 	case PS3_MATCH_ID_STOR_ROM:
 	case PS3_MATCH_ID_STOR_FLASH:
-	case PS3_MATCH_ID_STOR_NOR_FLASH:
 		return ps3_open_hv_device_sb(dev);
 
 	case PS3_MATCH_ID_SOUND:
@@ -172,7 +171,6 @@ int ps3_open_hv_device(struct ps3_system_bus_device *dev)
 
 	case PS3_MATCH_ID_AV_SETTINGS:
 	case PS3_MATCH_ID_SYSTEM_MANAGER:
-	case PS3_MATCH_ID_DISP_MANAGER:
 		pr_debug("%s:%d: unsupported match_id: %u\n", __func__,
 			__LINE__, dev->match_id);
 		pr_debug("%s:%d: bus_id: %llu\n", __func__, __LINE__,
@@ -203,7 +201,6 @@ int ps3_close_hv_device(struct ps3_system_bus_device *dev)
 	case PS3_MATCH_ID_STOR_DISK:
 	case PS3_MATCH_ID_STOR_ROM:
 	case PS3_MATCH_ID_STOR_FLASH:
-	case PS3_MATCH_ID_STOR_NOR_FLASH:
 		return ps3_close_hv_device_sb(dev);
 
 	case PS3_MATCH_ID_SOUND:
@@ -212,7 +209,6 @@ int ps3_close_hv_device(struct ps3_system_bus_device *dev)
 
 	case PS3_MATCH_ID_AV_SETTINGS:
 	case PS3_MATCH_ID_SYSTEM_MANAGER:
-	case PS3_MATCH_ID_DISP_MANAGER:
 		pr_debug("%s:%d: unsupported match_id: %u\n", __func__,
 			__LINE__, dev->match_id);
 		pr_debug("%s:%d: bus_id: %llu\n", __func__, __LINE__,

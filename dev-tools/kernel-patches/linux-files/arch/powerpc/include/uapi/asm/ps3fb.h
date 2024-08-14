71a3b361de940dac80ae1d3b8933617e08889919
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-14 23:27:01 +0200

    Restore vanilla linux files

--- dev-tools/kernel-patches/linux-files/arch/powerpc/include/uapi/asm/ps3fb.h
+++ dev-tools/kernel-patches/linux-files/arch/powerpc/include/uapi/asm/ps3fb.h
@@ -30,13 +30,6 @@
 #define PS3FB_IOCTL_ON            _IO('r', 4)        /* use IOCTL_FSEL */
 #define PS3FB_IOCTL_OFF           _IO('r', 5)        /* return to normal-flip */
 #define PS3FB_IOCTL_FSEL          _IOW('r', 6, int)  /* blit and flip request */
-#define PS3FB_IOCTL_GPU_SETUP     _IO('r', 7)         /* enable FIFO access */
-#define PS3FB_IOCTL_GPU_INFO      _IOR('r', 8, int)   /* get GPU info */
-#define PS3FB_IOCTL_GPU_ATTR      _IOW('r', 9, int)   /* set attribute */
-
-#define PS3FB_IOCTL_CURSOR_ENABLE _IOW('r', 10, int)	/* cursor enable/disable */
-#define PS3FB_IOCTL_CURSOR_POS    _IOW('r', 11, int)	/* cursor x/y pos*/
-#define PS3FB_IOCTL_CURSOR_OFFS   _IOW('r', 12, int)	/* cursor data offset */
 
 #ifndef FBIO_WAITFORVSYNC
 #define FBIO_WAITFORVSYNC         _IOW('F', 0x20, __u32) /* wait for vsync */
@@ -48,23 +41,6 @@ struct ps3fb_ioctl_res {
 	__u32 xoff; /* margine x  */
 	__u32 yoff; /* margine y */
 	__u32 num_frames; /* num of frame buffers */
-};
-
-struct ps3fb_ioctl_gpu_info {
-	__u32 vram_size;      /* size of available video memory */
-	__u32 fifo_size;      /* size of command buffer */
-	__u32 ctrl_size;      /* size of dma control registers */
-	__u32 dinfo_size;     /* size of driver info */
-	__u32 reports_size;   /* size of reports */
-	__u32 device_size[8]; /* size of gpu devices */
-};
-
-struct ps3fb_ioctl_gpu_attr {
-	__u64 attr;           /* attribute */
-	__u64 p0;             /* 1st parameter */
-	__u64 p1;             /* 2nd parameter */
-	__u64 p2;             /* 3rd parameter */
-	__u64 p3;             /* 4th parameter */
 };
 
 #endif /* _ASM_POWERPC_PS3FB_H_ */

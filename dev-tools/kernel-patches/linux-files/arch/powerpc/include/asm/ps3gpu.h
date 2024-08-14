8ca9fccf7c6a2816dbdc82d1bf7fbabd18914ac2
Damian Dudycz <damiandudycz@yahoo.com>
2024-08-14 23:31:54 +0200

    Patch linux

--- dev-tools/kernel-patches/linux-files/arch/powerpc/include/asm/ps3gpu.h
+++ dev-tools/kernel-patches/linux-files/arch/powerpc/include/asm/ps3gpu.h
@@ -23,6 +23,10 @@
 
 #define L1GPU_FB_BLIT_WAIT_FOR_COMPLETION	(1ULL << 32)
 
+#define L1GPU_CONTEXT_ATTRIBUTE_FIFO_SETUP	0x1
+#define L1GPU_CONTEXT_ATTRIBUTE_FIFO_PAUSE	0x2
+#define L1GPU_CONTEXT_ATTRIBUTE_FIFO_RESUME	0x3
+
 #define L1GPU_DISPLAY_SYNC_HSYNC		1
 #define L1GPU_DISPLAY_SYNC_VSYNC		2
 
@@ -32,19 +36,19 @@ extern struct mutex ps3_gpu_mutex;
 
 
 static inline int lv1_gpu_display_sync(u64 context_handle, u64 head,
-				       u64 ddr_offset)
+				       u64 sync_mode)
 {
 	return lv1_gpu_context_attribute(context_handle,
 					 L1GPU_CONTEXT_ATTRIBUTE_DISPLAY_SYNC,
-					 head, ddr_offset, 0, 0);
+					 head, sync_mode, 0, 0);
 }
 
 static inline int lv1_gpu_display_flip(u64 context_handle, u64 head,
-				       u64 ddr_offset)
+				       u64 sync_mode)
 {
 	return lv1_gpu_context_attribute(context_handle,
 					 L1GPU_CONTEXT_ATTRIBUTE_DISPLAY_FLIP,
-					 head, ddr_offset, 0, 0);
+					 head, sync_mode, 0, 0);
 }
 
 static inline int lv1_gpu_fb_setup(u64 context_handle, u64 xdr_lpar,

0f76e9d497fde7ed48e84ff3b49d4e1bcf6f34ff
damiandudycz <damiandudycz@yahoo.com>
2024-10-14 10:49:34 +0200

    Release - version 1.3.0 (#20)
    * Moving to catalyst-lab - wip
    
    * small fixes
    
    * Add catalyst-lab as a submodule
    
    * New release
    
    ---------
    
    Co-authored-by: root <root@localhost.localdomain>

--- dev-tools/environment/setup-06-releng.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ../../.env-shared.sh || exit 1
-
-empty_directory "${PATH_RELENG}"
-
-# Download and setup releng
-git clone -o upstream https://github.com/gentoo/releng.git "${PATH_RELENG}"

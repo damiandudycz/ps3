34242fbd1869da6344b5305e049fe96d8863c74f
Damian Dudycz <damiandudycz@yahoo.com>
2024-10-17 01:30:55 +0000

    Updates

--- dev-tools/environment/setup-04-catalyst.sh
+++ dev-tools/environment/setup-04-catalyst.sh
@@ -12,7 +12,7 @@ for PATCH in "${PATH_CATALYST_PATCHES_SRC}"/*.patch; do
 done
 
 # Install Catalyst
-unmask_package "dev-util/catalyst" "**"
+unmask_package "dev-util/catalyst"
 unmask_package "sys-fs/squashfs-tools-ng" # Unmask arch mask only.
 use_set_package "sys-apps/util-linux" "python"
 emerge dev-util/catalyst --newuse --update --deep

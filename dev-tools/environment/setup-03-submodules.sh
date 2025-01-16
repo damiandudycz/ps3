171815a30a17fbd9a1567ca423de3378571eac6b
Damian Dudycz <damiandudycz@yahoo.com>
2025-01-16 07:09:17 +0000

    WIP

--- dev-tools/environment/setup-03-submodules.sh
+++ dev-tools/environment/setup-03-submodules.sh
@@ -8,21 +8,3 @@ cd "${PATH_ROOT}"
 git submodule foreach 'git config submodule.$name.depth 1'
 git submodule update --init --recursive
 git submodule foreach 'git checkout main'
-
-# Setup LFS for autobuilds.
-cat <<EOF > "${PATH_GIT_HOOK_RELEASES}"
-# Remove binhost packages that are too large.
-for dir in "${PATH_RELEASES_PS3_GENTOO_ARCH_BINPACKAGES_PROFILE}"/{.[!.]*,*}; do
-    echo "[Sanitize: \$dir]"
-    cd "$(dirname "${PATH_BINHOST_SCRIPT_SANITIZE}")"
-    ${PATH_BINHOST_SCRIPT_SANITIZE} -p \${dir}
-    cd \${dir}
-    git add -u
-    echo ""
-done
-EOF
-chmod +x "${PATH_GIT_HOOK_RELEASES}"
-
-echo "[Initialize GIT LFS for releases]"
-cd "${PATH_ROOT}/releases"
-git lfs install
